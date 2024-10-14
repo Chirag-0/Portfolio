923207bb31d461f358a7988d7068ffa513b5969b
Chirag-0 <123808423+Chirag-0@users.noreply.github.com>
2024-10-14 16:22:11 +0530

    initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ import { useEffect } from "react"
 const projectInfo = [
   {
     name:"Movies Mania",
-    img:"/public/moviemania.png",
+    img:"/moviemania.png",
     details:"Developed a Movie Mania app using React JS, utilizing the OMDB API to fetch movie data.Implemented custom hooks for localStorage to persist watched data.",
     techStack:"Tech Stack: HTML,CSS,React JS",
     source:"Source Code",
@@ -18,7 +18,7 @@ const projectInfo = [
   },
   {
     name:"Notes App",
-    img:"/public/notesapp.png",
+    img:"/notesapp.png",
     details:"Developed a notes app with add, delete, and edit functionality, including timestamp display for creation and last modification. It is fully responsive and uses local storage for data persistence.",
     techStack:"Tech Stack: HTML,CSS,JavaScript",
     source:"Source Code",
@@ -26,7 +26,7 @@ const projectInfo = [
   },
   {
     name:"Snake Game",
-    img:"/public/snake.png",
+    img:"/snake.png",
     details:"Classic snake game with movement in four directions to eat food, tracking score and high score. Includes music on/off buttons to enhance user experience and customizable gameplay settings.",
     techStack:"Tech Stack: HTML,CSS,Javascipt",
     source:"Source Code",
@@ -34,7 +34,7 @@ const projectInfo = [
   },
   {
     name:"Weather App",
-    img:"/public/weather.png",
+    img:"/weather.png",
     details:"Weather app with search functionality to fetch and display city weather, including temperature, wind speed, and humidity. Fully responsive for a seamless experience across all devices.",
     techStack:"Tech Stack: HTML,CSS,Javascipt",
     source:"Source Code",
@@ -42,7 +42,7 @@ const projectInfo = [
   },
   {
     name:"File Compressor",
-    img:"/public/filecompressor.png",
+    img:"/filecompressor.png",
     details:"File compressor and decompressor in Java using the Huffman coding algorithm. Huffman coding is a popular algorithm for lossless data compression, commonly used in file compression applications.",
     techStack:"Tech Stack: Java",
     source:"Source Code",
@@ -50,7 +50,7 @@ const projectInfo = [
   },
   {
     name:"Cynthia Ugw Clone",
-    img:"/public/cynthiaugw.png",
+    img:"/cynthiaugw.png",
     details:"This project is a clone of the animated website Cynthia Ugw.The website features stunning animations and showcases my skills in frontend web development.",
     techStack:"Tech Stack: HTML,CSS,JavaScript,GSAP",
     source:"Source Code",
