e181816fce85d18869df070790e11ed928921436
Chirag-0 <123808423+Chirag-0@users.noreply.github.com>
2024-10-15 23:17:32 +0530

    Loader modification

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,11 +1,29 @@
+import { useEffect, useState } from "react"
 
-function Loader() {
+function Loader({ setLoading }) {
+   const [count,setCount] = useState(0);
+
+   useEffect(()=>{
+     const interval = setInterval(()=>{
+        setCount((prev) => {
+            if(prev<100){
+                return prev + 1;
+            }else{
+                clearInterval(interval);
+                setLoading(false);
+                return prev;
+            }
+        })
+     },40);
+
+     return () => clearInterval(interval);
+   },[setLoading]);
   return (
     <div className="bg-[#181818] flex flex-col gap-5 items-center justify-center h-screen">
-        <div className="w-16 h-16 border-4 border-t-4 border-gray-200 rounded-full animate-spin border-t-blue-400"></div>
+        <h2 className="font-titan text-white text-lg">{count}</h2>
         <h4 className="font-titan text-white animate-bounce">Loading...</h4>
     </div>
   )
 }
 
-export default Loader
+export default Loader;
