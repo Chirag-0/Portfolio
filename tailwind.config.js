2e39052a3881e481f825252ad9dbcc88d2d304d6
Chirag-0 <123808423+Chirag-0@users.noreply.github.com>
2024-10-17 18:52:52 +0530

    customized breakpoint added

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,15 @@
 /** @type {import('tailwindcss').Config} */
+import defaultTheme from 'tailwindcss/defaultTheme';
 export default {
   content: [
     "./index.html",
     "./src/**/*.{js,jsx}",
   ],
   theme: {
+    screens: {
+      ...defaultTheme.screens,
+      '3xl': '1740px',
+    },
     extend: {
       fontFamily: {
         'titan': ['Titan One', 'sans-serif'],
