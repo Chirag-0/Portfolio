199601166df016c9e97dad669a3ea8c3c6b08a88
Chirag-0 <123808423+Chirag-0@users.noreply.github.com>
2024-10-18 23:49:23 +0530

    margin bottom added

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -36,7 +36,7 @@ function Skills() {
     return (
       <div className="h-auto w-full bg-[#181818] mt-52 sm:mt-14 md:mt-6 lg:-mt-14">
         <div className="text-white md:mt-4 md:m-3 pt-20 ml-1 md:ml-12 font-bold text-5xl p-[2rem]">
-          <p ref={skills[0]} className="font-titan text-4xl md:text-6xl mt-24 md:mt-2">Skills</p>
+          <p ref={skills[0]} className="font-titan text-4xl md:text-6xl mt-24 md:mt-2 mb-8">Skills</p>
           <div className="w-[90%] font-roboto font-thin text-gray-400">
             {['HTML', 'CSS', 'JavaScript', 'React JS', 'Java', 'SQL', 'GSAP', 'Locomotive'].map((skill, i) => (
               <div key={i} ref={skills[i + 1]} className="border-t p-6 ">
