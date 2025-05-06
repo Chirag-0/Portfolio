64a62b3546dc944e618b48947c6b9310e8679ca8
Chirag-0 <123808423+Chirag-0@users.noreply.github.com>
2025-05-07 00:01:57 +0530

    initial commit

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -25,7 +25,6 @@ function Skills() {
               trigger: skill.current,
               start: "top 70%",
               end: "bottom 10%",
-              scrub: 1,
               markers: false,
             },
           }
@@ -37,7 +36,7 @@ function Skills() {
       <div className="h-auto w-full bg-[#181818] mt-52 sm:mt-14 md:mt-6 lg:-mt-14">
         <div className="text-white md:mt-4 md:m-3 pt-20 ml-1 md:ml-12 font-bold text-5xl p-[2rem]">
           <p ref={skills[0]} className="font-titan text-4xl md:text-6xl mt-24 md:mt-2 mb-8">Skills</p>
-          <div className="w-[90%] font-roboto font-thin text-gray-400">
+          <div className="w-[96%] font-roboto font-normal text-gray-500">
             {['HTML', 'CSS', 'JavaScript', 'React JS', 'Java', 'SQL', 'GSAP', 'Locomotive'].map((skill, i) => (
               <div key={i} ref={skills[i + 1]} className="border-t p-6 ">
                 {skill}
