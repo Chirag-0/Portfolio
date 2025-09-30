a936a39795942b09e1739960dc7e97b8a1e03947
Chirag-0 <123808423+Chirag-0@users.noreply.github.com>
2025-09-30 18:17:36 +0530

    import fixed

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import Contact from "./components/Contact"
 import Hero from "./components/Hero"
 import Navbar from "./components/Navbar"
 import Projects from "./components/Projects"
-import Skills from './component/Skills';
+import Skills from './components/Skills';
 import Loader from "./components/Loader";
 import { createBrowserRouter, RouterProvider,useLocation } from "react-router-dom"
 import { useEffect,useState} from "react"
